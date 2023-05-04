ef78327225be9a6082099d0ad931f744bf10d7ea
pragati-sriv <ps028ideal@gmail.com>
2023-05-04 08:51:47 +0530

    progress

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,15 +1,14 @@
-// import logo from './logo.svg';
 import './App.css';
-import Card  from './molecule/molecule';
+// import Handle from './component/atom/atom';
+import Primary from './molecule/molecule';
 
 function App() {
   return (
     <div className="App">
-     <Card/>
+        <Primary/>
+     {/* <Handle/> */}
     </div>
   );
 }
 
 export default App;
-
-
--- myapp/src/molecule/molecule.js
+++ myapp/src/molecule/molecule.js
@@ -1,50 +1,34 @@
-import './molecule.css'
-import {useState,useEffect} from 'react'
-export default function Card(){
-    const[data ,setData]=useState([])
+import { useState } from "react"
+// import Handle from "../component/atom/atom"
+export default function Primary()
+{
 
-    
-    
-
-  useEffect(()=>{
-    // fetch('https://reqres.in/api/users/')
-    // .then((data)=>data.json())
-    // .then((response)=>setData(response.data))
-    // .catch((error=>console.log(error)))
-    
-  },[])
-
-
- function customClick()
- {
-    fetch('https://reqres.in/api/users/')
-    .then((data)=>data.json())
-    .then((response)=>setData(response.data))
-    .catch((error=>console.log(error)))
- }
-
-
-
-
+const [mobile,setMobile]=useState()
+  
+function save()
+{
+    // console.log(mobile)
+    let user={mobile}     // {mobile}
+    fetch("https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP",{
+        method:"POST",
+        headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(user)
+    }).then((result)=>{
+        result.json().then((resp)=>{
+           console.log("resp",resp)
+        })
+    })
+}
 
     return(
-         <>
-
-     {/* <h1 className='design'>click</h1> */}
-
-     <button className="click" onClick={customClick} > Get Users</button>
-     <div className='cardContainer'>
-    {data.map((users)=>(
-        <div>
-         <h3>{users.id}</h3>   
-        <h3>{users.first_name}</h3>
-        <h3>{users.last_name}</h3>
-        <p>{users.email}</p>
-        <img src={users.avatar}  alt="avatar"/>
-        </div>
-    ))}
-
-     </div>
-     </>
+        <>
+        <h3>Generate Otp </h3>
+        <input type="text" placeholder="Number" onChange={(e)=>{setMobile(e.target.value)}}/>
+        <button onClick={save}>Get Otp</button>
+        {/* <Handle/> */}
+        </>
     )
 }
