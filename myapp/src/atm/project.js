634da14d6d1817368b6fa770193276e70f527da7
pragati-sriv <ps028ideal@gmail.com>
2023-05-17 09:37:19 +0530

    in pogress

---
+++ myapp/src/atm/project.js
@@ -0,0 +1,8 @@
+export default function Project() {
+    return (
+      <div>
+        <h1>I am Project Page</h1>
+      </div>
+    );
+  }
+  
---
+++ myapp/src/atm/video.js
@@ -0,0 +1,8 @@
+export default function Video() {
+    return (
+      <div>
+        <h1>I am Video Page</h1>
+      </div>
+    );
+  }
+  
