4f3be7c3d7163dfb5440ae820e030148552dc64e
pragati-sriv <ps028ideal@gmail.com>
2023-05-29 23:16:13 +0530

    in progress

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import LoginPage from "./pages/loginPage";
+import AdminLogin from "./pages/adminLogin";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/adminlogin" element={<AdminLogin />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
